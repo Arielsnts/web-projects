faff0ca56b04f9b567c42514908852a7638b86d4
Ariel Santos <165219923+Arielsnts@users.noreply.github.com>
2025-08-02 17:41:32 -0300

    update

--- metal-blog/src/app/posts/[slug]/page.tsx
+++ metal-blog/src/app/posts/[slug]/page.tsx
@@ -2,33 +2,29 @@ import { alta, recentes } from '../../../data/posts'
 import styles from './page.module.css'
 
 interface PostPageProps {
-    params: {
-        slug: string;
-    }
+  params: {
+    slug: string;
+  };
 }
 
-export default function PostPage({ params }: PostPageProps) {
-    const slug = params.slug;
+export default async function Page({ params }: PostPageProps) {
+  const slug = params.slug;
 
-    const allPosts = alta.concat(recentes);
-    const post = allPosts.find(post => post.slug === slug);
+  const allPosts = alta.concat(recentes);
+  const post = allPosts.find(post => post.slug === slug);
 
-    return (
-        <div className={styles.principal}>
-            <h1>{post?.title}</h1>
-            <h2>{post?.subtitle}</h2>
-            <img src={post?.coverImage} alt="cover" />
-            <p>{post?.album} - {post?.date}</p>
-            <div className={styles.text}>
-                <p>{post?.content}</p>
-                <h2>Titulo</h2>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet commodi earum dignissimos odio, nostrum alias quis quasi? Vitae molestiae, eveniet, maiores tempore magnam sunt, cumque dolorem pariatur incidunt id expedita. Lorem ipsum dolor sit amet consectetur, adipisicing elit. At quae, modi explicabo atque repellat quam hic rerum nobis beatae sequi aliquid dolore animi molestiae, repellendus aperiam nihil quod veniam quos?</p>
-                <h2>Titulo</h2>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet commodi earum dignissimos odio, nostrum alias quis quasi? Vitae molestiae, eveniet, maiores tempore magnam sunt, cumque dolorem pariatur incidunt id expedita. Lorem ipsum dolor sit amet consectetur, adipisicing elit. At quae, modi explicabo atque repellat quam hic rerum nobis beatae sequi aliquid dolore animi molestiae, repellendus aperiam nihil quod veniam quos?</p>
-                <h2>Titulo</h2>
-                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet commodi earum dignissimos odio, nostrum alias quis quasi? Vitae molestiae, eveniet, maiores tempore magnam sunt, cumque dolorem pariatur incidunt id expedita. Lorem ipsum dolor sit amet consectetur, adipisicing elit. At quae, modi explicabo atque repellat quam hic rerum nobis beatae sequi aliquid dolore animi molestiae, repellendus aperiam nihil quod veniam quos?</p>
-                <a href="/" className={styles.voltar}>VOLTAR</a>
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.principal}>
+      <h1>{post?.title}</h1>
+      <h2>{post?.subtitle}</h2>
+      <img src={post?.coverImage} alt="cover" />
+      <p>{post?.album} - {post?.date}</p>
+      <div className={styles.text}>
+        <p>{post?.content}</p>
+        <h2>Titulo</h2>
+        <p>...</p>
+        <a href="/" className={styles.voltar}>VOLTAR</a>
+      </div>
+    </div>
+  );
 }
